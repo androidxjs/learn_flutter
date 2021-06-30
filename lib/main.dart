5813d70265e8e70baa992e3dd03ad5c5b672edcf
xjs <xujingshuai@shihuituan.com>
2021-06-30 17:46:04 +0800

    leading、action

--- lib/main.dart
+++ lib/main.dart
@@ -10,35 +10,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false ,
       home: MyHomePage(),
       theme: ThemeData(primarySwatch: Colors.yellow),
-      // theme: ThemeData(
-      //   // This is the theme of your application.
-      //   //
-      //   // Try running your application with "flutter run". You'll see the
-      //   // application has a blue toolbar. Then, without quitting the app, try
-      //   // changing the primarySwatch below to Colors.green and then invoke
-      //   // "hot reload" (press "r" in the console where you ran "flutter run",
-      //   // or simply save your changes to "hot reload" in a Flutter IDE).
-      //   // Notice that the counter didn't reset back to zero; the application
-      //   // is not restarted.
-      //   primarySwatch: Colors.blue,
-      // ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation' ,
+          onPressed: ()=>debugPrint('点击navigation按钮'),
+        ),
+        title: Text('flutter你好'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'serch' ,
+            onPressed: ()=>debugPrint('点击search按钮'),
+          ),
+        ],
+        elevation: 8.0,
+      ),
+      body:ListViewDemo(),
+    );
+  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+}
+class ListViewDemo extends StatelessWidget{
   Widget _listItemBuilder(BuildContext context, int index) {
     return Container(
       color: Colors.white,
@@ -61,20 +66,13 @@ class MyHomePage extends StatelessWidget {
       ),
     );
   }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('flutter你好'),
-        elevation: 8.0,
-      ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: _listItemBuilder,
-      ),
+    return  ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: _listItemBuilder,
     );
   }
+
 }
 
--- lib/post.dart
+++ lib/post.dart
@@ -13,27 +13,27 @@ final List<Post> posts=[
   Post(
     title: 'Candy Shop',
     author: 'Mohamed Chahin',
-    imageUrl: 'https://resources.ninghao.org/images/candy-shop.jpg',
+    imageUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202005%2F20%2F20200520024521_zkkfs.jpg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1627634946&t=aac8914eb09e2088e13c7731db03fcaa',
   ),
   Post(
     title: 'Childhood in a picture',
     author: 'Mohamed Chahin',
-    imageUrl: 'https://resources.ninghao.org/images/childhood-in-a-picture.jpg',
+    imageUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fwww.v3wall.com%2Fwallpaper%2F1280_800%2F1301%2F1280_800_20130110065136424279.jpg&refer=http%3A%2F%2Fwww.v3wall.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1627634973&t=a22ebfe503403e5cbc3b746211e28a52',
   ),
   Post(
     title: 'Contained',
     author: 'Mohamed Chahin',
-    imageUrl: 'https://resources.ninghao.org/images/contained.jpg',
+    imageUrl: 'https://img2.baidu.com/it/u=4150176662,621307066&fm=26&fmt=auto&gp=0.jpg',
   ),
   Post(
     title: 'Dragon',
     author: 'Mohamed Chahin',
-    imageUrl: 'https://resources.ninghao.org/images/dragon.jpg',
+    imageUrl: 'https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/zhidao/pic/item/adaf2edda3cc7cd968cd92783801213fb90e91ab.jpg',
   ),
   Post(
     title: 'Free Hugs',
     author: 'Mohamed Chahin',
-    imageUrl: 'https://resources.ninghao.org/images/free_hugs.jpg',
+    imageUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202006%2F09%2F20200609131631_mjbrp.jpg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1627634993&t=5473b65970562aabd093f7dbb5a30f5f',
   ),
   Post(
     title: 'Gravity Falls',
